f055fcbf55a25816143cb37c4a364ea7495821d0
michboon <michboon@email>
2014-03-03 19:40:25 +0000

    Various small fixes

--- www/js/home.js
+++ www/js/home.js
@@ -11,5 +11,14 @@ function getPosts() {
 }
 
 function prepare() {
-
+    var sessionUser;
+    $.ajaxSetup({async:false});
+    $.getJSON( "/api/currentUser", function(data) {
+        sessionUser = data["username"];
+    });
+    $.ajaxSetup({async:true});
+    $("#newPostForm").submit(function(e){
+        e.preventDefault();
+        addPost($("#newPostForm :input").val(), sessionUser);
+    });
 }
--- www/js/timeline.js
+++ www/js/timeline.js
@@ -11,30 +11,10 @@ function getPosts() {
     });
 }
 
-function addPost(content) {
-    var currUsername = window.location.pathname.split( '/' )[2];
-    $.ajax({
-        url: "/api/user/" + currUsername,
-        type: "POST",
-        data: {content: content},
-
-        success: function(response) {
-            var data = $.parseJSON(response);
-            $.each( data, function(key, val) {
-                if(key == "error")
-                    displayModal(val);
-                else if(key == "result" && val == "added") {
-                    $("#newPostForm")[0].reset();
-                }
-            });
-            getPosts();
-        }
-    });
-}
-
 function prepare() {
     $("#newPostForm").submit(function(e){
         e.preventDefault();
-        addPost($("#newPostForm :input").val());
+        var currUsername = window.location.pathname.split( '/' )[2];
+        addPost($("#newPostForm :input").val(), currUsername);
     });
 }
